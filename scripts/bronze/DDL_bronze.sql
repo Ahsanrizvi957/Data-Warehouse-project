f2ec71a60925bbf227b5fed8ef5db04584284827
Ahsan_rizvi <ahsanrizvi31@gmail.com>
2026-04-15 21:34:10 +0500

    Update DDL_bronze.sql

--- scripts/bronze/DDL_bronze.sql
+++ scripts/bronze/DDL_bronze.sql
@@ -14,12 +14,12 @@ Script Purpose:
 DROP TABLE IF EXISTS bronze.crm_cust_info; 
 CREATE TABLE bronze.crm_cust_info(
 
-	cst_id   		       INT,
-	cst_key  		       VARCHAR(50),
+	cst_id   		   INT,
+	cst_key  		   VARCHAR(50),
 	cst_firstname      VARCHAR(50),
-	cst_lastname 	     VARCHAR(50),
+	cst_lastname 	   VARCHAR(50),
 	cst_marital_status VARCHAR(50),
-	cst_gndr 		       VARCHAR(50),
+	cst_gndr 		   VARCHAR(50),
 	cst_create_date    DATE
 
 );
@@ -33,8 +33,8 @@ CREATE TABLE bronze.crm_prd_info(
 	prd_nm 			  VARCHAR (50),
 	prd_cost 		  INT,
 	prd_line 		  VARCHAR (50),
-	prd_start_dt  DATE,
-	prd_end_dt 		DATE
+	prd_start_dt  	  DATE,
+	prd_end_dt 		  DATE
 
 );
 -- Create bronze.crm_sales_details if not already exists 
@@ -75,9 +75,9 @@ CREATE TABLE bronze.erp_LOC_A101(
 DROP TABLE IF EXISTS bronze.erp_PX_CAT_G1V2;
 CREATE TABLE bronze.erp_PX_CAT_G1V2(
 
-	ID 			    VARCHAR (50),
-	CAT 		    VARCHAR (50),
-	SUBCAT 		  VARCHAR (50),
+	ID 			VARCHAR (50),
+	CAT 		VARCHAR (50),
+	SUBCAT 		VARCHAR (50),
 	MAINTENANCE VARCHAR (50)
   
 );
